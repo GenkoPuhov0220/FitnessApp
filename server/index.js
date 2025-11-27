630f83c308e3adbc1bce4752760ce7445dd060f0
GenkoPuhov0220 <genkopuhov2002@gmail.com>
2025-11-27 10:24:56 +0200

    path

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
-app.options("/api/*", cors());
+app.options("/:path(*)", cors());
 
 // --- Connect to MongoDB ---
 mongoose.connect(process.env.MONGO_URI)
