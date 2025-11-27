b326c0ce24f4969d236e2df05f47491f9b3c6fad
GenkoPuhov0220 <genkopuhov2002@gmail.com>
2025-11-27 09:54:04 +0200

    cors bug

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  optionsSuccessStatus: 204,
+  optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 //app.options("/*", cors(corsOptions));
